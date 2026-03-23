84d64dd8a174e45c9847b70af1c0d11010865117
NKU100 <21164383+NKU100@users.noreply.github.com>
2026-03-23 19:41:24 +0800

    chore: upgrade android.jar from API 28 to API 36
    Upgrade android.jar to API 36 to resolve 'attr not found' warnings for
    newer attributes (requestLegacyExternalStorage, foregroundServiceType,
    preserveLegacyExternalStorage, etc.).
    
    Also add hidden (@hide) platform attribute IDs that are not present in
    any public android.jar (compileSdkVersion, compileSdkVersionCodename,
    appComponentFactory, usesNonSdkApi, etc.). These are aapt2-written
    manifest metadata attributes.

--- src/main/java/android/content/res/Resources.java
+++ src/main/java/android/content/res/Resources.java
@@ -10,6 +10,19 @@ public class Resources {
 
     public HashMap<String, Integer> attrMap = new HashMap<>();
 
+    /**
+     * Hidden (@hide) platform attributes that are NOT in the public android.jar
+     * but are commonly found in APK manifests (written by aapt2).
+     * Resource IDs sourced from AOSP frameworks/base/core/res/res/values/public.xml
+     */
+    private static final Object[][] HIDDEN_ATTRS = {
+        {"compileSdkVersion",           0x01010572},
+        {"compileSdkVersionCodename",   0x01010573},
+        {"appComponentFactory",         0x0101057a},
+        {"usesNonSdkApi",               0x01010578},
+        {"allowCrossUidActivitySwitchFromBelow", 0x01010680},
+    };
+
     private void init() {
         try {
             Field[] fs = android.R.attr.class.getFields();
@@ -18,6 +31,10 @@ public class Resources {
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+        // Add hidden platform attrs that are missing from public android.jar
+        for (Object[] entry : HIDDEN_ATTRS) {
+            attrMap.putIfAbsent((String) entry[0], (Integer) entry[1]);
         }
     }
 
